c55fd5fb64cd9987fbbea073322a42d3d2ca1afd
MahendraKhibrah <mahendrakrs448@gmail.com>
2023-12-15 17:33:57 +0700

    update example in domain models

--- domain/lib/models/example.dart
+++ domain/lib/models/example.dart
@@ -1,11 +1,23 @@
+import 'dart:convert';
+
 import 'package:common/common.dart';
 
 part 'example.freezed.dart';
+part 'example.g.dart';
 
 @Freezed(makeCollectionsUnmodifiable: false)
 class Example with _$Example {
   const factory Example({
-    required int id,
-    required String name,
+    @JsonKey(name: 'id') required String? id,
+    @JsonKey(name: 'type') required String? type,
+    @JsonKey(name: 'setup') required DateTime? setup,
+    @JsonKey(name: 'punchline') required String? punchline,
   }) = _Example;
+
+  factory Example.fromJson(
+    Map<String, dynamic> json,
+  ) =>
+      _$ExampleFromJson(json);
+
+  factory Example.fromRawJson(String str) => Example.fromJson(json.decode(str));
 }
